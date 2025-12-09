2698e5333d0907be5392992b71a03e3402c680bd
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-10 00:00:11 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <Button>Day 0</Button>
+    </div>
+  )
+}
+
+export default App
