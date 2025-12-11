ddc9475cb7e88fb15ffa61a2dea0b6e3a91b55b0
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-11 23:56:04 +0200

    add a hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/hero-page";
 import Navbar from "./components/navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   )
 }
---
+++ src/components/hero-page.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+    return (
+      <section className="bg-primary h-screen">
+            
+            <div className="h-screen items-center flex justify-center">
+                <h1 className="text-5xl font-extrabold text-secondary">First Page</h1></div>
+      </section>
+  )
+}
+
+export default HeroSection
