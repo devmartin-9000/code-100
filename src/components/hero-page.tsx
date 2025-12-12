6bf9f84a0fa18d6c62097b2171acac74f5b47f01
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-12 23:56:00 +0200

    add image

--- src/components/hero-page.tsx
+++ src/components/hero-page.tsx
@@ -1,11 +1,12 @@
 const HeroSection = () => {
     return (
-      <section className="bg-primary h-screen">
-            
-            <div className="h-screen items-center flex justify-center">
-                <h1 className="text-5xl font-extrabold text-secondary">First Page</h1></div>
-      </section>
-  )
+			<section className="h-screen">
+				<img  className="w-full fixed" src="./hero-image.jpg" alt="Hero Image" />
+				<div className="w-full items-center flex justify-center">
+					<h1 className="text-5xl font-extrabold text-secondary">First Page</h1>
+				</div>
+			</section>
+		);
 }
 
 export default HeroSection
