e4e4559c1bad4af9670f3d2e502d0c514eb62401
devmartin-9000 <dev.martinjack9000@gmail.com>
2025-12-15 00:02:12 +0200

    add hover states for nav menu items

--- src/components/hero-page.tsx
+++ src/components/hero-page.tsx
@@ -1,9 +1,9 @@
 const HeroSection = () => {
     return (
 			<section className="h-screen">
-				<img  className="w-full fixed" src="./hero-image.jpg" alt="Hero Image" />
-				<div className="w-full items-center flex justify-center">
-					<h1 className="text-5xl font-extrabold text-secondary">First Page</h1>
+				<img  className="w-full fixed bg-cover" src="./hero-image.jpg" alt="Hero Image" />
+				<div className="relative w-full z-50 items-center flex justify-center">
+					<h1 className="text-5xl font-extrabold text-secondary"></h1>
 				</div>
 			</section>
 		);
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,43 @@
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
 		<div className="fixed z-50 inset-0 p-2 bg-primary/10">
-          {/* LEFT NAV */}
-          <div className="bg-secondary rounded-4xl">
-              <div className="flex items-center gap-4 border-s-10 border-e-10 border-primary p-2 rounded-full">
-                  {/* Logo */}
-                  <img src="./site-logo.svg" alt="Site Logo" className="h-8 w-auto" />
-                  {/* Text */}
-				<h1 className="text-primary font-extrabold text-3xl">code100</h1>
+			{/* LEFT NAV */}
+			<div className="bg-secondary rounded-4xl">
+				<div className="flex items-center justify-between border-s-10 border-e-10 border-primary p-2 rounded-full">
+					<div className="flex gap-4 items-center">
+						{/* Logo */}
+						<img src="./site-logo.svg" alt="Site Logo" className="h-8 w-auto" />
+						{/* Text */}
+						<h1 className="text-primary font-extrabold text-3xl">code100</h1>
+					</div>
+					<div className="flex gap-4 text-md font-bold">
+						{/* Menu Links */}
+						<a className="border-y-3 p-2 rounded-full border-transparent hover:border-primary transform-border transition-all duration-500 cursor-pointer">
+							Games
+						</a>
+						<a className="border-y-3 p-2 rounded-full border-transparent hover:border-primary transform-border transition-all duration-500 cursor-pointer">
+							Tools
+						</a>
+						<a className="border-y-3 p-2 rounded-full border-transparent hover:border-primary transform-border transition-all duration-500 cursor-pointer">
+							Blogs
+						</a>
+						<a className="border-y-3 p-2 rounded-full border-transparent hover:border-primary transform-border transition-all duration-500 cursor-pointer">
+							Random
+						</a>
+					</div>
+					<div className="flex gap-4">
+						<Button
+							className="w-36 rounded-full bg-transparent hover:border-primary/70 transform transition-all duration-200"
+							variant="outline"
+						>
+							Log In
+						</Button>
+						<Button className="w-36 rounded-full">Create Account</Button>
+					</div>
+				</div>
 			</div>
-          </div>
 		</div>
 	);
 }
